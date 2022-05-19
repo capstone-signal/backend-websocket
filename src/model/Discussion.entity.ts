85ac277437c62eae2e53dda3d3b3185a3e186692
Donghyeong Kang <dh@uos.ac.kr>
2022-05-19 10:32:26 +0900

    FIX: discussion user relation

--- src/model/Discussion.entity.ts
+++ src/model/Discussion.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, OneToMany, PrimaryColumn } from "typeorm";
+import { Entity, JoinColumn, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
 import { DiscussionCode } from "./DiscussionCode.entity";
+import { User } from "./User.entity";
 
 @Entity({name: "discussion"})
 export class Discussion {
@@ -8,4 +9,13 @@ export class Discussion {
 
 	@OneToMany(() => DiscussionCode, (discussionCode) => discussionCode.discussion)
 	discussionCodes: DiscussionCode[];
+
+	@ManyToOne(
+		() => User,
+		{
+			eager: true
+		}
+	)
+	@JoinColumn({ name: "user_id" })
+	user: User;
 }
