5ea5ec751a06d5e72ba351be608afb544b0d227d
Donghyeong Kang <dh@uos.ac.kr>
2022-05-19 10:32:45 +0900

    CAPS137/ websocket handler 추가

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,18 +1,61 @@
+import { ConfigService } from '@nestjs/config';
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Request } from 'express';
 import { Server , Socket} from 'socket.io';
+import { AuthService } from 'src/service/auth/auth.service';
+import { ReservationService } from 'src/service/reservation/reservation.service';
+import { EventsService } from './events.service';
 const WebSocket = require('ws');
 const yWebsocketUtils = require('y-websocket/bin/utils');
 
-var socketsStatus= {};
-
-@WebSocketGateway(process.env.WEBSOCKET_PORT as unknown as number, {transports: ['websocket']}) // TODO : config service for externalization config
+@WebSocketGateway(1235, {transports: ['websocket']}) // TODO : config service for externalization config
 export class EventsGateway {
-@WebSocketServer() public server : Server;
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly reservationService: ReservationService,
+    private readonly authService: AuthService,
+    private readonly eventsService: EventsService
+  ) {}
+
+  @WebSocketServer() public server : Server;
 
   afterInit(server: Server) {
-    console.log('Websocketserver init');
-    server.addListener('connection', yWebsocketUtils.setupWSConnection);
+    server.addListener('connection', async (conn: WebSocket, req: Request) => {
+      // authentciation based on req.headers.cookie, review id -> is user is reviewer or reviewee
+      try {
+        const { reservationId, reviewDiffId } = this.eventsService.parseUrl(req.url.split('/')[1]);
+
+        const reservation = await this.reservationService.findById(reservationId);
+        if(!reservation) {
+          throw new Error('Reservation not found');
+        }
+
+        const cookies = req.headers.cookie.split(';');
+        const accessToken = cookies.find(cookie => cookie.split('=')[0].trim() === 'accessToken').split('=')[1];
+        if(!accessToken) {
+          throw new Error('Access token not found');
+        }
+
+        const userId = this.authService.validateToken(accessToken);
+        if(!userId || userId != reservation.reviewer.id && userId != reservation.discussion.user.id) {
+          throw new Error('Invalid user');
+        }
+
+        if(!this.reservationService.isProcessing(reservation)) {
+         // throw new Error('Reservation is not processing');
+        }
+
+        // init phase 1  : y websocket based on docName
+        const docName = req.url.split('/')[1];
+        // 만약 문서가 없으면 초기화 해야함
+        yWebsocketUtils.setupWSConnection(conn, req, { docName, gc: true });
+      } catch (e) {
+        console.error(e);
+        conn.close();
+      }
+    });
   }
+
 
   //연결 되었을때
   handleConnection(@ConnectedSocket() socket: WebSocket): any {
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/service/auth/auth.module';
+import { ReservationModule } from 'src/service/reservation/reservation.module';
 import { EventsGateway } from './events.gateway';
+import { EventsService } from './events.service';
 
 @Module({
-    providers : [EventsGateway],
+    imports: [ConfigModule, ReservationModule, AuthModule],
+    providers : [EventsGateway, EventsService],
 })
 export class EventsModule {}
