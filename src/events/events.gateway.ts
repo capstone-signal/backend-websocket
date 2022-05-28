626ad18e16c1ce5b0f2f0df3a8694f57a8f5f6ef
Donghyeong Kang <dh@uos.ac.kr>
2022-05-28 15:12:27 +0900

    CAPS137/ ws message handler & url

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -10,7 +10,9 @@ import Y from 'yjs';
 const WebSocket = require('ws');
 const yWebsocketUtils = require('y-websocket/bin/utils');
 
-@WebSocketGateway(1235, {transports: ['websocket']}) // TODO : config service for externalization config
+const SYNC_PERIOD = 10;
+const WSS_PORT = 3001;
+@WebSocketGateway(WSS_PORT, {transports: ['websocket']}) // TODO : config service for externalization config
 export class EventsGateway {
   constructor(
     private readonly configService: ConfigService,
@@ -24,9 +26,8 @@ export class EventsGateway {
 
   afterInit(server: Server) {
     server.addListener('connection', async (conn: WebSocket, req: Request) => {
-      // authentciation based on req.headers.cookie, review id -> is user is reviewer or reviewee
       try {
-        const { reservationId, reviewDiffId } = this.eventsService.parseUrl(req.url.split('/')[1]);
+        const { reservationId, reviewDiffId } = this.eventsService.parseUrl(req.url.split('/socket/')[1]);
 
         const reservation = await this.reservationService.findById(reservationId);
         if(!reservation) {
@@ -48,24 +49,25 @@ export class EventsGateway {
          // throw new Error('Reservation is not processing');
         }
 
-        const docName = req.url.split('/')[1];
+        const docName = req.url.split('/socket/')[1];
 
         // init phase
         const ydoc = yWebsocketUtils.getYDoc(docName);
         const text = ydoc.getText(docName) as Y.Text;
         if(this.isNotInitialized(text)) {
           const { codeAfter } = await this.reviewService.getReviewDiff(reviewDiffId);
+          console.log(codeAfter);
           text.insert(0, codeAfter, {});
         }
-        console.log(text.toString())
-       
         yWebsocketUtils.setupWSConnection(conn, req, { docName, gc: true });
-        // shutdown phase
-        // conn.addEventListener('message', (event) => {
-        //   if(!this.reservationService.isProcessing(reservation)) {
-        //     this.handleCloseConnection(conn);
-        //   }
-        // })
+        conn.addEventListener('message', (event) => {
+          if(!this.reservationService.isProcessing(reservation)) {
+            this.handleCloseConnection(conn); // 1. 세션 종료 시 연결 끊기
+          }
+          if(event.timeStamp % SYNC_PERIOD === 0) { // 2. 랜덤하게 데이터 동기화 시켜주기 TODO NoSQL로 성능개선
+            console.log(text.toString());
+          }
+        })
       } catch (e) {
         console.error(e);
         conn.close();
@@ -87,6 +89,7 @@ export class EventsGateway {
   }
 
   handleCloseConnection(connection: WebSocket): any {
+    console.log("close connection");
     connection.close(1000);
   }
 }
