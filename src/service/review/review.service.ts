28116fb83cf94e675b7ffc13958f1035b40f6060
Donghyeong Kang <dh@uos.ac.kr>
2022-05-30 14:00:33 +0900

    CAPS137/ review complete service

--- src/service/review/review.service.ts
+++ src/service/review/review.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { LiveReviewDiff } from "src/model/LiveReviewDiff.entity";
+import { Review } from "src/model/Review.entity";
 import { Repository, UpdateResult } from "typeorm";
 
 @Injectable()
 export class ReviewService {
 	  constructor(
 		  @InjectRepository(LiveReviewDiff)
-		  private readonly liveReviewDiffRepository: Repository<LiveReviewDiff>
+		  private readonly liveReviewDiffRepository: Repository<LiveReviewDiff>,
+		  @InjectRepository(Review)
+		  private readonly reviewRepository: Repository<Review>
 	  ) {}
 	  async getReviewDiff(reviewDiffId: number): Promise<LiveReviewDiff> {
         return this.liveReviewDiffRepository.findOne(reviewDiffId);
@@ -17,5 +20,9 @@ export class ReviewService {
 		reviewDiff.codeAfter = codeAfter;
 		return this.liveReviewDiffRepository.update(reviewDiff.id, reviewDiff);
 	  }
+
+	  async complete(reviewReservation): Promise<void> {
+		  reviewReservation.review.isDone = true;
+		  await this.reviewRepository.save(reviewReservation.review);
 	  }
 }
